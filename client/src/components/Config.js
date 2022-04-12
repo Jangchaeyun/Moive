b9b0800e8ab3627d5fe8ee32ac2ec0f4aced7643
Jangchaeyun <62280276+Jangchaeyun@users.noreply.github.com>
2022-04-12 16:04:39 +0900

    master

---
+++ client/src/components/Config.js
@@ -0,0 +1,6 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = 'fc8e1f5de82a7ad028916b2c9db3228a';
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
